64b765ac9d03a7fd577dd7c842ee1c0cdac0f5ca
Jomar Milan <jomarm@jomarm.com>
2025-06-16 19:30:47 -0700

    Allow encryption in groups using tags

--- src/independent-encrypt/widgets/encryption-agent.ts
+++ src/independent-encrypt/widgets/encryption-agent.ts
@@ -1,5 +1,44 @@
 import { widget as Widget } from '$:/core/modules/widgets/widget.js';
-import { type IParseTreeNode, type IWidgetEvent, type IWidgetInitialiseOptions } from 'tiddlywiki';
+import {
+  type IParseTreeNode,
+  type IWidgetEvent,
+  type IWidgetInitialiseOptions,
+  type Wiki,
+} from 'tiddlywiki';
+
+function encryptTiddler(
+  wiki: Wiki,
+  title: string,
+  password: string,
+  noticeOverride?: string,
+) {
+  const tiddler = wiki.getTiddler(title);
+  if (tiddler === undefined) return;
+
+  const encrypted = $tw.crypto.encrypt(wiki.getTiddlerAsJson(title), password);
+
+  const keptFields = Object.fromEntries(
+    Object.entries(tiddler.fields).filter(([key]) =>
+      [
+        'title',
+        'tags',
+        'modified',
+        'modifier',
+        'created',
+        'creator',
+        'encrypt-notice',
+      ].includes(key),
+    ),
+  );
+  if (noticeOverride) keptFields['encrypt-notice'] = noticeOverride;
+  const newFields = {
+    text: keptFields['encrypt-notice'],
+    encrypted,
+    ...keptFields,
+  };
+
+  wiki.addTiddler(new $tw.Tiddler(newFields));
+}
 
 class EncryptionAgent extends Widget {
   constructor(
@@ -12,41 +51,31 @@ class EncryptionAgent extends Widget {
       {
         type: 'tw-encrypt-tiddler',
         handler: (_event: IWidgetEvent) => {
+          const currentTitle = this.getVariable('currentTiddler');
           const formDataTiddler = this.wiki.getTiddler(
-            `$:/temp/encrypt-form/${this.getVariable('currentTiddler')}`,
+            `$:/temp/encrypt-form/${currentTitle}`,
           );
           const password = formDataTiddler?.fields.password;
           if (typeof password !== 'string') return true;
 
-          const currentTiddler = this.wiki.getTiddler(
-            this.getVariable('currentTiddler'),
-          );
-          if (currentTiddler === undefined) return true;
-          const encrypted = $tw.crypto.encrypt(
-            this.wiki.getTiddlerAsJson(this.getVariable('currentTiddler')),
-            password,
+          const currentTiddler = this.wiki.getTiddler(currentTitle);
+          const group = currentTiddler?.fields.tags?.findLast(tag =>
+            tag.startsWith('$:/tags/EncryptGroup/'),
           );
 
-          const keptFields = Object.fromEntries(
-            Object.entries(currentTiddler.fields).filter(([key]) =>
-              [
-                'title',
-                'tags',
-                'modified',
-                'modifier',
-                'created',
-                'creator',
-                'encrypt-notice',
-              ].includes(key)
-            ),
-          );
-          const newFields = {
-            text: keptFields['encrypt-notice'],
-            encrypted,
-            ...keptFields,
-          };
-
-          this.wiki.addTiddler(new $tw.Tiddler(newFields));
+          if (group === undefined) {
+            encryptTiddler(this.wiki, currentTitle, password);
+          } else {
+            const notice = currentTiddler?.fields['encrypt-notice'];
+            this.wiki.getTiddlersWithTag(group).forEach(title => {
+              encryptTiddler(
+                this.wiki,
+                title,
+                password,
+                typeof notice === 'string' ? notice : undefined,
+              );
+            });
+          }
 
           return false;
         },
